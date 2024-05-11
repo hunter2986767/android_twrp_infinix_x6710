16a15bdd92d7887c971c2b6c4c78ebb5cc45f470
hunter2986767 <156557124+hunter2986767@users.noreply.github.com>
2024-05-11 23:06:58 +0800

    Update twrp_X6710.mk

--- twrp_X6739.mk
+++ twrp_X6739.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from Infinix-X6739 device
+# Inherit from Infinix-X6710 device
 $(call inherit-product, device/infinix/X6739/device.mk)
 
 # Inherit from those products. Most specific first.
@@ -29,17 +29,17 @@ $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 # Enable updating of APEXes
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-PRODUCT_DEVICE := X6739
-PRODUCT_NAME := twrp_X6739
+PRODUCT_DEVICE := X6710
+PRODUCT_NAME := twrp_X6710
 PRODUCT_BRAND := Infinix
-PRODUCT_MODEL := Infinix GT 10 Pro
+PRODUCT_MODEL := Infinix Note 30 VIP
 PRODUCT_MANUFACTURER := infinix
 
 PRODUCT_GMS_CLIENTID_BASE := android-infinix
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_x6739_h931-user 12 SP1A.210812.016 537496 release-keys" \
-    TARGET_DEVICE=Infinix-X6739 \
-    PRODUCT_NAME=X6739-GL
+    PRIVATE_BUILD_DESC="vnd_infinix-user 13 TP1A.220624.014 217628 release-keys" \
+    TARGET_DEVICE=Infinix-X6710 \
+    PRODUCT_NAME=X6710-GL
 
-BUILD_FINGERPRINT := "Infinix/X6739-GL/Infinix-X6739:12/SP1A.210812.016/240204V1917:user/release-keys"
+BUILD_FINGERPRINT := "Infinix/X6710-GL/Infinix-X6710:12/SP1A.210812.016/230111V331:user/release-keys"
